362332760dc8579bc1c783ff01f4aa7bdd66b3a2
neggs <littleneggs@gmail.com>
2019-03-22 14:49:13 +0000

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -2,6 +2,12 @@
 
 # Replace the hostname in the container
 sed -i.bak 's/HOSTNAME/'"$HOSTNAME"'/g' /www/data/index.html
+# Replace the hostname in the container
+
+PODDESC="$(kubectl describe service)"
+
+
+sed -i.bak 's/PODDESC/'"$PODDESC"'/g' /www/data/index.html
 
 # Startup the cmd
 exec "$@"
