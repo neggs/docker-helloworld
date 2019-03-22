68f132c4a0b765f8128b5a0346d446378a81d67b
neggs <littleneggs@gmail.com>
2019-03-22 15:51:15 +0000

    Update runner.sh

--- runner.sh
+++ runner.sh
@@ -6,7 +6,7 @@ sed -i.bak 's/HOSTNAME/'"$HOSTNAME"'/g' /www/data/index.html
 
 PODDESC="$(kubectl describe service)"
 
-sed -i.bak 's/PODDESC/'"$PODDESC"'/g' /www/data/index.html
+sed -i.bak 's/PODDESC/'$PODDESC'/g' /www/data/index.html
 
 # Startup the cmd
 exec "$@"
